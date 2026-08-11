d7792ec457882670c020b26ab49975e1533c7a95
nhanty00234-crypto <nhanty00234@gmail.com>
2026-08-11 21:16:31 +0700

    ádasd

--- sql/migration_avatar_url.sql
+++
@@ -1,7 +0,0 @@
--- migration_avatar_url.sql
--- Thêm cột AvatarUrl vào bảng Accounts. Idempotent: chạy lại không lỗi.
-
-IF COL_LENGTH('Accounts', 'AvatarUrl') IS NULL
-    ALTER TABLE Accounts ADD AvatarUrl NVARCHAR(500) NULL;
-
-PRINT 'Migration AvatarUrl hoàn tất.';
